d32b048ecc53ad714e44d2b4151abda43546c407
Nuo <xv.nuo@icloud.com>
2025-09-21 01:08:32 +0000

    dataloader done

--- vae/dataset.py
+++ vae/dataset.py
@@ -11,8 +11,10 @@ class BimodalDataset(Dataset):
         self.met_tr_df = pd.read_csv(config_d['data_fp'])
         self.num_samples = len(self.met_tr_df.index)
         mask = self.met_tr_df.columns.str.contains(r'^ENSG') # Split df into met data and tra data
-        self.tr_df = self.met_tr_df.loc[:, mask] # DataFrame with transcriptomics data
-        self.met_df = self.met_tr_df.loc[:, ~mask] # DataFrame with metabolomics data
+        self.tr_df = self.met_tr_df.loc[:, self.met_tr_df.columns[mask]] # DataFrame with transcriptomics data
+        self.met_df = self.met_tr_df.loc[:, self.met_tr_df.columns[~mask]] # DataFrame with metabolomics data
+        self.num_met = len(self.met_df.columns)
+        self.num_tr = len(self.tr_df.columns)
         ...
         
     def __len__(self) -> int:
@@ -28,18 +30,27 @@ class BimodalDataset(Dataset):
         }
         """
         # Extract desired row
-        # DataFrame with transcriptomics data
         tr_df = self.tr_df.loc[idx]
 
         # DataFrame with metabolomics data
         met_df = self.met_df.loc[idx]
+        
+        """# DataFrame with transcriptomics data
+        tr_df = self.tr_df.loc[[idx]]
+
+        # DataFrame with metabolomics data
+        met_df = self.met_df.loc[[idx]]"""
 
         # Extract the CCLE_ID
         ccle_id = met_df['CCLE_ID']
 
-        # Remove the CCLE_ID and DepMap_ID cols
-        met_df.pop('CCLE_ID')
-        met_df.pop('DepMap_ID')
+        """# Remove the CCLE_ID and DepMap_ID cols
+        if 'CCLE_ID' in met_df.columns:
+            met_df.pop('CCLE_ID')
+
+        if 'DepMap_ID' in met_df.columns:
+            met_df.pop('DepMap_ID')"""
+        print(f"met df: {met_df}")
 
         return {'name': ccle_id, 'met': torch.tensor(list(met_df)), 'tra': torch.tensor(list(tr_df))}
         ...
--- vae/losses.py
+++ vae/losses.py
@@ -1,22 +1,22 @@
 import torch.nn.functional as F
 import torch
 
-def bimodal_beta_vae_loss(recon_x1, recon_x2, x1, x2, mu, logvar, config_d):
+def bimodal_beta_vae_loss(recon_x1, recon_x2, x1, x2, mu, logvar, beta, lambda_1, lambda_2):
 
     total_loss = 0
     # Reconstruction loss for modality 1
     if x1 is not None:
         x1_flat = x1.view(x1.size(0), -1)
         bce_1 = F.binary_cross_entropy(recon_x1, x1_flat, reduction='sum')
-        total_loss += config.lambda_1 * bce_1
+        total_loss += lambda_1 * bce_1
     
     # Reconstruction loss for modality 2  
     if x2 is not None:
         x2_flat = x2.view(x2.size(0), -1)
         bce_2 = F.binary_cross_entropy(recon_x2, x2_flat, reduction='sum')
-        total_loss += config.lambda_2 * bce_2
+        total_loss += lambda_2 * bce_2
     
     # KL divergence loss (same regardless of modalities)
     kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     
-    return total_loss + config.beta * kld
+    return total_loss + beta * kld
